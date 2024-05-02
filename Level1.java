422f0ce2461243f133c9305a8ae1a2b21ca01d92
Been777 <o7.been@gmail.com>
2024-05-03 07:03:32 +0900

    Lv1 - 3

--- Level1.java
+++ Level1.java
@@ -12,7 +12,31 @@ public class Level1 {
         System.out.println("사칙연산 기호를 입력하세요: ");
         char operator = scanner.next().charAt(0);
 
-        System.out.println("operator: " + operator);
+        int result = 0;
 
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("올바른 선택이 아닙니다.");
+                break;
+        }
+
+        System.out.println("결과 : " + result);
     }
 }
